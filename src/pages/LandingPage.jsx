8cfac1ae4258c68e3d34c3b39520e731ff02d4c9
Endale2 <endale406@gmail.com>
2024-09-02 18:32:48 +0300

    updated

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function LandingPage() {
   return (
     <div>
       <h1>Welcome to the Landing Page</h1>
       <p>This is the entry point of the app.</p>
+      <div>
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/register">
+          <button>Register</button>
+        </Link>
+      </div>
     </div>
   );
 }
